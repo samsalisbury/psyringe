d94a499c819b8144239bc1e3d3bb915ccaf95b18
Sam Salisbury <samsalisbury@gmail.com>
2016-03-13 16:43:36 +0000

    Add tests for Fill()

---
+++ syringe_test.go
@@ -0,0 +1,106 @@
+package syringe
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
+)
+
+type named string
+
+func TestFill_Success(t *testing.T) {
+	err := New().Fill(
+
+		// objects
+		//
+		"a plain string",                      // string
+		int(256),                              // int
+		float64(256),                          // float64
+		float32(256),                          // float32
+		named("string"),                       // named
+		&bytes.Buffer{},                       // *bytes.Buffer{}
+		func() (int, int) { return 1, 2 },     // func() (int, int)
+		func() (error, int) { return nil, 1 }, // func() (error, int)
+
+		// constructors
+		//
+		func() io.Reader { return nil },               // io.Reader
+		func() (io.Writer, error) { return nil, nil }, // io.Writer
+		func() error { return nil },                   // error
+	)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+}
+
+func TestFill_AlreadyRegisteredError(t *testing.T) {
+	e := func(a, b interface{}, expectedInjectionType string) {
+		err := New().Fill(a, b)
+		what := fmt.Sprintf("Fill(%T, %T)", a, b)
+		if err == nil {
+			t.Errorf("nil error for", what)
+		}
+		expectedError := fmt.Sprintf("injection type %s already registered",
+			expectedInjectionType)
+		errMessage := err.Error()
+		if !strings.Contains(errMessage, expectedError) {
+			t.Errorf("got error %q for %s; want %q", err.Error(),
+				what, expectedError)
+		}
+	}
+
+	// objects
+	//
+	e("hello", "world", "string")
+	e(1, 2, "int")
+	e(func() {}, func() {}, "func()")
+	e(struct{}{}, struct{}{}, "struct {}")
+	e(
+		func() (string, string) { return "", "" },
+		func() (string, string) { return "hello", "world" },
+		"func() (string, string)",
+	)
+	// This one illustrates that for non constructors,
+	// only the value of the item counts, not any particular
+	// interface it is asserted as.
+	e(
+		io.Reader(&bytes.Buffer{}).(io.Reader),
+		io.Writer(&bytes.Buffer{}).(io.Writer),
+		"*bytes.Buffer",
+	)
+
+	// constructors
+	//
+	e(
+		func() int { return 1 },
+		func() int { return 1 },
+		"int",
+	)
+	e(
+		func() int { return 1 },
+		func() (int, error) { return 1, nil },
+		"int",
+	)
+	e(
+		func() (int, error) { return 1, nil },
+		func() int { return 1 },
+		"int",
+	)
+	e(
+		func() interface{} { return nil },
+		func() interface{} { return nil },
+		"interface {}",
+	)
+	e(
+		func() (interface{}, error) { return nil, nil },
+		func() interface{} { return nil },
+		"interface {}",
+	)
+	e(
+		func() interface{} { return nil },
+		func() (interface{}, error) { return nil, nil },
+		"interface {}",
+	)
+}
