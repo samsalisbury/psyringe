797b0aa92ef44780e1fc811ef96aebf82eefa806
Sam Salisbury <samsalisbury@gmail.com>
2016-07-24 18:00:28 +0100

    test counter: use int not uint

--- counter_test.go
+++ counter_test.go
@@ -5,23 +5,23 @@ import (
 	"sync/atomic"
 )
 
-// Counter wraps a uint64 and provides methods for atomically incrementing and
+// Counter wraps an int64 and provides methods for atomically incrementing and
 // reading that value.
 type Counter struct {
-	value uint64
+	value int64
 }
 
 // Increment atomically adds one to the counter.
 func (c *Counter) Increment() {
-	atomic.AddUint64(&c.value, 1)
+	atomic.AddInt64(&c.value, 1)
 }
 
 // Value atomically gets the current value of the counter.
-func (c *Counter) Value() uint64 {
-	return atomic.LoadUint64(&c.value)
+func (c *Counter) Value() int64 {
+	return atomic.LoadInt64(&c.value)
 }
 
 // String atomically returns the current value as a base 10 integer.
 func (c *Counter) String() string {
-	return strconv.FormatUint(c.Value(), 10)
+	return strconv.FormatInt(c.Value(), 10)
 }
